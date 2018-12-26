9bf28e5d4b9a9a69a66709115366ff334ac2dd3d
Savanni D'Gerinel <savanni@luminescent-dreams.com>
2018-12-25 23:33:40 -0500

    Housekeeping

--- shell.nix
+++ shell.nix
@@ -1,18 +1,15 @@
 let
-    pkgs = import <stable> {};
-    frameworks = pkgs.darwin.apple_sdk.frameworks;
+    pkgs = import <nixpkgs-18.09> {};
+    unstable = import <nixpkgs> {};
 in pkgs.stdenv.mkDerivation {
     name = "emseries";
 
     buildInputs = [ pkgs.rustc
                     pkgs.cargo
-                    frameworks.Security
-                    frameworks.CoreFoundation
-                    frameworks.CoreServices
+                    unstable.carnix
                   ];
 
     shellHook = ''
         export PS1="[$name] \[$txtgrn\]\u@\h\[$txtwht\]:\[$bldpur\]\w \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty \[$bldylw\]\$aws_env\[$txtrst\]\$ "
-        export NIX_LDFLAGS="-F${frameworks.CoreFoundation}/Library/Frameworks -framework CoreFoundation $NIX_LDFLAGS";
     '';
 }
